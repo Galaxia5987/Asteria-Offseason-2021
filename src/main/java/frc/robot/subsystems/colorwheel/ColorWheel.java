9976b8b5a6f4415c91fd1bd4c34f3e531d0bea42
Saar <saarzi2004@gmail.com>
2021-09-12 18:02:40 +0300

    Sunday

--- src/main/java/frc/robot/subsystems/colorwheel/ColorWheel.java
+++ src/main/java/frc/robot/subsystems/colorwheel/ColorWheel.java
@@ -9,19 +9,24 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.ColorSensorV3;
 
+import static frc.robot.Pot.ColorWheel.*;
+
 public class ColorWheel extends SubsystemBase {
-    VictorSPX motor = new VictorSPX(0);
+    VictorSPX motor = new VictorSPX(PORT_MOTOR);
     ColorSensorV3 sensor = new ColorSensorV3(I2C.Port.kMXP);
     ColorMatch match = new ColorMatch();
 
     private String lastColor = "";
 
-    public String whatColor() {
-        Color color = sensor.getColor();
+    public ColorWheel() {
         match.addColorMatch(Color.kYellow); // yellow
         match.addColorMatch(Color.kGreen); // green
         match.addColorMatch(Color.kRed); // red
         match.addColorMatch(Color.kBlue); // blue
+    }
+
+    public String whatColor() {
+        Color color = sensor.getColor();
         ColorMatchResult result = match.matchClosestColor(color);
         Color resultColor = result.color;
 
@@ -35,13 +40,14 @@ public class ColorWheel extends SubsystemBase {
             return "blue";
         }
     }
-        public String rotationColor () {
-            return "";
-        }
 
-        public void setPower (double powerMotor){
-            motor.set(ControlMode.PercentOutput, powerMotor);
-        }
-
-
+    public String rotationColor() {
+        return "";
     }
+
+    public void setPower(double powerMotor) {
+        motor.set(ControlMode.PercentOutput, powerMotor);
+    }
+
+
+}
