c88b4404c10e7d1a3969d8981e7ed899b7007431
littlebro12 <eitan.vichik@gmail.com>
2021-09-14 15:22:50 +0300

    Documentation
    Documentation for the Shooter and Fire classes

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -9,6 +9,15 @@ import frc.robot.subsystems.UnitModel;
 import static frc.robot.Ports.Shooter.*;
 import static frc.robot.Constants.Shooter.*;
 
+/**
+ * This is the class used to do all necessary calculations
+ * for the shooter
+ *
+ * motorMain - the leaing motor, all calculations are done
+ * based on this motor
+ *
+ * motorAux1/2 - these two motors follow motorMain
+ */
 public class Shooter extends SubsystemBase {
     public static double targetDistance;
     private final TalonSRX motorMain = new TalonSRX(portMain);
@@ -16,6 +25,10 @@ public class Shooter extends SubsystemBase {
     private final TalonSRX motorAux2 = new TalonSRX(portAux2);
     private final UnitModel unitMan = new UnitModel(TICKS);
 
+    /**
+     * Here the constants for the PID is set for the TalonSRX
+     * computer, and the aux motors are set to follow
+     */
     public Shooter() {
         motorMain.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
         motorAux1.follow(motorMain);
@@ -25,15 +38,28 @@ public class Shooter extends SubsystemBase {
         motorMain.config_kD(0, kD);
     }
 
+
+    /**
+     * This function gets the velocity from the TalonSRX,
+     * and converts it to the required units
+     * @return the velocity in rpm
+     */
     public double getVelocity(){
         double ticks100ms = motorMain.getSelectedSensorVelocity();
         return unitMan.toVelocity(ticks100ms);
     }
 
+    /**
+     * This function simply sets the speed for the motor to get to
+     * @param velocity is the target velocity for the motors
+     */
     public void setVelocity(double velocity){
         motorMain.set(ControlMode.Velocity, unitMan.toTicks100ms(velocity));
     }
 
+    /**
+     * This program stops the motors
+     */
     public void terminate(){
         motorMain.set(ControlMode.PercentOutput, 0);
     }
--- src/main/java/frc/robot/subsystems/shooter/commands/Fire.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/Fire.java
@@ -3,37 +3,74 @@ package frc.robot.subsystems.shooter.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.shooter.Shooter;
 
+/**
+ * This class fires the shooter
+ * sniper - this variable is the shooter
+ * distance - this is the target distance
+ * targetSpeed - this is the speed the motors
+ * need to get to
+ */
 public class Fire extends CommandBase {
     private Shooter sniper;
     private double distance;
     private double targetSpeed;
 
+    /**
+     * Constructor
+     * @param sniper is the Shooter
+     * @param distance is the speed needed
+     */
     public Fire(Shooter sniper, double distance) {
         this.sniper = sniper;
         this.distance = distance;
         addRequirements(sniper);
     }
 
+    /**
+     * This function simply gets the value
+     * for the target speed
+     */
     @Override
     public void initialize() {
         targetSpeed = getTargetSpeed(distance);
     }
 
+    /**
+     * This function runs in a loop and sets
+     * the velocity required according to the
+     * Shooter class function - setVelocity
+     */
     @Override
     public void execute() {
         sniper.setVelocity(targetSpeed);
     }
 
+    /**
+     * This finishes the program
+     * @param interrupted is self explanatory
+     */
     @Override
     public void end(boolean interrupted) {
         sniper.terminate();
     }
 
+    /**
+     * While the program is running,
+     * it will not be finished and hence:
+     * @return is always false
+     */
     @Override
     public boolean isFinished() {
         return false;
     }
 
+    /**
+     * This function gets the target speed
+     * required for the motors according to
+     * the distance the ball needs to travel
+     * @param distance is how far the ball will fly
+     * @return the speed in rpm
+     */
     public double getTargetSpeed(double distance){
         return 0;
     }
