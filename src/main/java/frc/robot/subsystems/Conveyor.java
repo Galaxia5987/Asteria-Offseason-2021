da0ba457670f968693bbf77f1b27f4f5fc3dfdd4
Gaia Zano <gaya.zano11@gmail.com>
2021-09-14 16:24:20 +0300

    Add motor, set motor power.

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -1,9 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-    public class Conveyor extends CommandBase{
-        public final motor
+public class Conveyor extends CommandBase {
+    public final TalonSRX motor = new TalonSRX(Ports.Conveyor.MOTOR_POWER);
 
+    private void setMotor (double power){
+        motor.set(ControlMode.PercentOutput, power);
     }
+
+    private void setMotorInverted(){
+        motor.setInverted(Ports.Conveyor.IS_MOTOR_INVERTED);
+    }
+}
 
--- src/main/java/frc/robot/subsystems/Ports.java
+++ src/main/java/frc/robot/subsystems/Ports.java
@@ -1,4 +1,9 @@
 package frc.robot.subsystems;
 
 public class Ports {
+    public class Conveyor{
+        public static final int MOTOR_POWER = 0;
+        public static final boolean IS_MOTOR_INVERTED = false;
+
+    }
 }
