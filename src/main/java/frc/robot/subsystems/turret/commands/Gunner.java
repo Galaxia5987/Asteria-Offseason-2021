8afb36a6abf4e7f21f919121377b8e62ae000d12
littlebro12 <eitan.vichik@gmail.com>
2021-09-17 10:51:37 +0300

    Set up buttons
    Setting up the buttons for Gunner class.

--- src/main/java/frc/robot/subsystems/turret/commands/Gunner.java
+++ src/main/java/frc/robot/subsystems/turret/commands/Gunner.java
@@ -1,23 +1,39 @@
 package frc.robot.subsystems.turret.commands;
 
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.*;
 import frc.robot.subsystems.turret.Turret;
 
 public class Gunner extends CommandBase {
+    private double X;
+    private double Y;
     private double targetAngle; // Angle the turret needs to be at.
-    private Turret gunnerMan = new Turret(); // Turret class object (in order to move the motor).
+    private Turret gunnerMan; // Turret class object (in order to move the motor).
+    private final XboxController xbox = new XboxController(1);
+    Trigger LT = new Trigger(() -> xbox.getRawAxis(XboxController.Axis.kLeftTrigger.value) > 0.3);
 
-    // Constructor.
-    public Gunner(double targetAngle){
-        this.targetAngle = targetAngle;
+    public Gunner(Turret gunnerMan){
+        this.gunnerMan = gunnerMan;
+        addRequirements(gunnerMan);
     }
 
     // Executes all the commands in a loop.
     @Override
     public void execute() {
+        X = xbox.getX(GenericHID.Hand.kRight);
+        Y = xbox.getY(GenericHID.Hand.kRight);
+        targetAngle = Math.atan2(Y, X);
+
+
         gunnerMan.setTargetAngle(targetAngle);
-        gunnerMan.setAngle();
-        gunnerMan.setCurrAngle(targetAngle);
+
+        if(LT.get())
+            gunnerMan.setAngle();
+
+        if (isFinished()) {
+            gunnerMan.setCurrAngle(targetAngle);
+        }
     }
 
     // Resets the turret.
@@ -29,6 +45,9 @@ public class Gunner extends CommandBase {
     // Continues running the program until interrupted.
     @Override
     public boolean isFinished() {
-        return false;
+        if (!gunnerMan.anglesEqual())
+            return false;
+        else
+            return true;
     }
 }
